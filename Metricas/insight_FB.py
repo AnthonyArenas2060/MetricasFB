8d0519af5311b0435d4a97d919c1116c90eb9e98
AnthonyArenas2060 <134089217+AnthonyArenas2060@users.noreply.github.com>
2025-12-01 10:47:33 -0600

    Enhance Facebook metrics insights with followers data
    Added functionality to display followers and posts insights from Facebook Pages, including city-wise follower counts.

--- Metricas/insight_FB.py
+++ Metricas/insight_FB.py
@@ -135,12 +135,26 @@ if user_long_token:
                     posteos["Shares"] = sh
                     st.subheader("📋 Post asociadas a tu cuenta")
                     st.dataframe(posteos)
+
+                    fans_city = graph.get_connections(id=page_id, connection_name = 'insights', metric = 'page_follows_city',
+                                  since = '2025-07-01', until = '2025-07-31')
+                    fans_city_df = pd.DataFrame.from_dict(fans_city['data'][0]['values'][0]['value'], orient = 'index')
+                    fans_city_df.reset_index(inplace=True)
+                    fans_city_df.rename(columns={'index': 'Ciudad', 0: 'Cantidad'}, inplace=True)
+                    st.bar_chart(
+                                fans_city_df['Ciudad'],
+                                y_label='Cantidad',
+                                use_container_width=True
+                            )
+
+                
                 else:
                     st.write("No se encontraron publicaciones en el rango de fechas seleccionado.")
 
 
     except Exception as e:
         st.error(f"Ocurrió un error: {e}")
+
 
 
 
